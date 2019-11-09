d6f7a0513798fd3f1a8040e7ee2f0d60fdc2ca24
idd juma otuya <jotuya@HOSOFTWARE02.familyfinance.co.ke>
2019-11-09 11:16:53 +0300

    landing page views

--- mains.js
+++ mains.js
@@ -5,7 +5,15 @@ const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
+app.set('view-engine', 'ejs');
+
+//home
 app.get('/', function(req, res){
- res.json({"OLA" : "Open Locations API"});
+    //res.json({"OLA" : "Open Locations API"});
+    res.render('index', {
+        title:'Open Locations API | OLA by juma',
+        page:'Home',
+        menuId:'home'
+    })
 });
 app.listen(port, function(){ console.log('OLA server running on port {port}');});
